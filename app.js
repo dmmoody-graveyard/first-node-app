2110964621d83fd5971ff614cc9b16e55a8613b8
Duane M. Moody <dmmoody@gmail.com>
2015-06-01 10:10:02 -0700

    add parse function

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-//Problem: We need a simple way to look at a user's badge count and javascript points on teamtreehouse.com
-//Solution: use Node.js to connect to Treehouse's API to get profile information.
 var http = require("http")
-var username = "duanemoody"
+var username = "duanemoody1"
 
 function printMessage(username, badgeCount, points) {
-	var message = username + " has " + badgeCount + " total badge(s) and " + points + " in JavaScript";
+	var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
 	console.log(message);
 }
 
+function printError(error) {
+  console.error(error.message);
+}
+
 var request = http.get("http://teamtreehouse.com/" + username + ".json", function(response) {
-	console.log(response.statusCode)
+  var body = "";
+  console.log(response.statusCode)
+  response.on('data', function(chunk) {
+    body += chunk;
+  });
+  response.on('end', function() {
+    if(response.statusCode === 200) {
+      try {
+        var profile = JSON.parse(body);
+        printMessage(username, profile.badges.length, profile.points.JavaScript);
+      } catch(error) {
+        printError(error);
+    }
+  });
 });
 
-request.on("error", function(error) {
-  console.error(error.message);
-});
+request.on("error", printError);
