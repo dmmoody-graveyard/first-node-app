cd4242e6b8842245fa142dd0cbdd293f6d9e5c2f
Duane M. Moody <dmmoody@gmail.com>
2015-06-10 09:54:02 -0700

    add better error handling

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-var http = require("http")
-var username = "duanemoody1"
+var profile = require("./profile")
 
-function printMessage(username, badgeCount, points) {
-	var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
-	console.log(message);
-}
-
-function printError(error) {
-  console.error(error.message);
-}
-
-var request = http.get("http://teamtreehouse.com/" + username + ".json", function(response) {
-  var body = "";
-  console.log(response.statusCode)
-  response.on('data', function(chunk) {
-    body += chunk;
-  });
-  response.on('end', function() {
-    if(response.statusCode === 200) {
-      try {
-        var profile = JSON.parse(body);
-        printMessage(username, profile.badges.length, profile.points.JavaScript);
-      } catch(error) {
-        printError(error);
-    }
-  });
-});
-
-request.on("error", printError);
+profile.get('duanemoody');
