de8c3681c0b0c81742e42f0930921a33861b28ce
Duane M. Moody <dmmoody@gmail.com>
2015-06-10 16:08:48 -0700

    fix heroku port number

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var http = require('http');
 http.createServer(function (request, response) {
   router.home(request, response);
   router.user(request, response);
-}).listen(5000, '127.0.0.1');
+}).listen(process.env.PORT || 5000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337');
