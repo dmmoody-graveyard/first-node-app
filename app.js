0316de665b435826b9b1300bc7698069cede031e
Duane M. Moody <dmmoody@gmail.com>
2015-06-10 11:26:59 -0700

    add webserver method

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-var profile = require("./profile");
-var users = process.argv.slice(2);
+var http = require('http');
+http.createServer(function (request, response) {
+  homeRoute(request, response);
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337');
 
-users.forEach(profile.get);
+function homeRoute(request, response) {
+  if(request.url === '/') {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write("Header\n");
+    response.write("Search\n");
+    response.end("footer\n");
+  }
+}
+
+
+
+// var profile = require("./profile");
+// var users = process.argv.slice(2);
+
+// users.forEach(profile.get);
