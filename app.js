ac2690a224b059206b2a39d9c74ecf0d2a1eb17b
Duane M. Moody <dmmoody@gmail.com>
2015-06-10 15:14:31 -0700

    change server

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 var router = require('./router');
 
 var http = require('http');
-http.createServer(function (request, response) {
-  router.home(request, response);
-  router.user(request, response);
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337');
-
-
-
-
-// var profile = require("./profile");
-// var users = process.argv.slice(2);
-
-// users.forEach(profile.get);
+// http.createServer(function (request, response) {
+//   router.home(request, response);
+//   router.user(request, response);
+// }).listen(1337, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:1337');
