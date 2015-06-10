46673e1bf96803d1ac8bda098d616f5745a381df
Duane M. Moody <dmmoody@gmail.com>
2015-06-10 15:24:30 -0700

    add json file for heroku

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var router = require('./router');
 
 var http = require('http');
-// http.createServer(function (request, response) {
-//   router.home(request, response);
-//   router.user(request, response);
-// }).listen(1337, '127.0.0.1');
-// console.log('Server running at http://127.0.0.1:1337');
+http.createServer(function (request, response) {
+  router.home(request, response);
+  router.user(request, response);
+}).listen(5000, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337');
